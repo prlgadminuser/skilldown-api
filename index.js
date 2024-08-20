4ee5a7cfb790c58e23a22d51fed1c6bb3c512be4
prlgadminuser <liam.heizmann@gmail.com>
2024-08-20 05:45:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -629,12 +629,12 @@ const user = await userCollection.findOne(
     );
  
     const itemshop1 = "dailyItems";
-const document = await shopcollection.findOne({ _id: itemshop1 }, { projection: { 'items.itemId': 1 }}
+const itemshop = await shopcollection.findOne({ _id: itemshop1 }, { projection: { 'items.itemId': 1 }}
 );
 
-    //  const selectedItem = Object.values(itemshop.items).find(i => i.itemId === itemId);
+    const selectedItem = Object.values(itemshop.items).find(i => i.itemId === itemId);
 
-    if (!document) {
+    if (!selectedItem) {
       return res.status(401).json({ message: "Item is not valid." });
       }
 
