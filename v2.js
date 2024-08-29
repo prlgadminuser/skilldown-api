a4c3c4d2c074e9fdd799af1fb0e1c1f78f6d694e
prlgadminuser <liam.heizmann@gmail.com>
2024-08-29 20:32:30 +0200

    Update v2.js

--- v2.js
+++ v2.js
@@ -66,7 +66,7 @@ const crypto = require('crypto');
 
 const webhookURL = process.env.DISCORD_KEY;
 const tokenkey = process.env.TOKEN_KEY;
-const cryptoKey = process.env.TOKEN_KEY;
+const cryptoKey = 'cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e';
 
 const eventEmitter = new EventEmitter();
 
