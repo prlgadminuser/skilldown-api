047ee821949d72b2808cc0813185a8d212674ec7
prlgadminuser <liam.heizmann@gmail.com>
2024-09-27 18:09:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const gift_dev_msg = "Gift from Liquem Games. Have fun! and rate the game on itch if you like it";
+const gift_dev_msg = "Gift from Liquem Games. Have fun!";
 const coinsmin = 20
 const coinsmax = 35
 const lobbytheme = 2
