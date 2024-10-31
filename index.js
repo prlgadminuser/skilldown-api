032e48d06cdab55de7097f4eaaff5542644909d0
prlgadminuser <liam.heizmann@gmail.com>
2024-10-31 01:28:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -690,12 +690,14 @@ if (containsBadWords) {
     }
 
     // Check account creation limit here
-    applyAccountCreationLimit(req, res, async () => {
+   
 
       
         const hashedPassword = await bcrypt.hash(password, 10);
         const token = jwt.sign({ username }, tokenkey);
         const currentTimestamp = new Date();
+
+     applyAccountCreationLimit(req, res, async () => {
 
         
 
