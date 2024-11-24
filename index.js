cfe200b826446abe11b5b06d59ac5a9590178395
prlgadminuser <liam.heizmann@gmail.com>
2024-11-24 19:28:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const gift_dev_msg = "Gift from Liquem Games";
 const rarity_normal = 0.8 //0.8
-const rarity_legendary = 0.9 //0.995
+const rarity_legendary = 0.999 //0.995
 const allgadgets = 3
 const friendMax = 30
 const maxaccountlimit = 6
